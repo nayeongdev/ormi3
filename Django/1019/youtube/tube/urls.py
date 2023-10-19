13266fd30086d9dee9f37897c546b918eef73f08
nayeongdev <nayeongkim.dev@gmail.com>
2023-10-19 17:04:21 +0900

    231019수업 - CBV에서 댓글구현(youtube) & 인증(account_auth)

---
+++ Django/1019/youtube/tube/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = "tube"
+
+urlpatterns = [
+    path("", views.post_list, name="post_list"),
+    path("<int:pk>/", views.post_detail, name="post_detail"),
+    path("new/", views.post_new, name="post_new"),
+    path("<int:pk>/edit/", views.post_edit, name="post_edit"),
+    path("<int:pk>/delete/", views.post_delete, name="post_delete"),
+    path("<int:pk>/post-comment", views.post_comment, name="post_comment"),
+]
---
+++ Django/1019/youtube/tube/views.py
@@ -0,0 +1,118 @@
+from typing import Any
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db import models
+from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.views.generic import (
+    ListView,
+    DeleteView,
+    UpdateView,
+    DetailView,
+    CreateView,
+)
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
+
+class PostListView(ListView):
+    model = Post
+    ordering = "-pk"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        q = self.request.GET.get("q", "")
+        if q:
+            return super().get_queryset().filter(title__icontains=q)
+        return super().get_queryset()
+
+
+post_list = PostListView.as_view()
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy("tube:post_list")
+    template_name = "tube/form.html"
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        video = form.save(commit=False)  # commit=False는 DB에 저장하지않고, 객체만 반환
+        video.author = self.request.user
+        return super().form_valid(form)  # 이렇게 호출했을 때 저장
+
+
+post_new = PostCreateView.as_view()
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        """
+        여기서 원하는 쿼리셋이나 object를 추가한 후 템플릿으로 전달할 수 있습니다.
+        """
+
+        context = super().get_context_data(**kwargs)
+        context["comment_form"] = CommentForm()
+
+        return context
+
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get("pk")
+        post = Post.objects.get(pk=pk)
+        post.view_count += 1
+        post.save()
+        return super().get_object(queryset)
+
+
+post_detail = PostDetailView.as_view()
+
+
+class PostUpdateView(UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy("tube:post_list")
+    template_name = "tube/form.html"
+
+    def test_func(self):
+        # UserPassesTestMixin에 있고 test_func() 메서드를 오버라이딩, True, False 값으로 접근 제한
+        return self.get_object().author == self.request.user
+
+
+post_edit = PostUpdateView.as_view()
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy("tube:post_list")
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+
+post_delete = PostDeleteView.as_view()
+
+
+@login_required
+def post_comment(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)  # commit=False는 DB에 저장하지 않고 객체만 반환
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect("tube:post_detail", pk)
+    else:
+        form = CommentForm()
+    return render(
+        request,
+        "tube/form.html",
+        {
+            "form": form,
+        },
+    )
